9bc17921fb49498f3240bd825c92b3b301f0fd0b
DOVAHKIIN404 <bmw5779@gmail.com>
2024-11-23 16:13:34 +0900

    player 정보 수정

--- textGameManager/src/Entities/Player.cpp
+++ textGameManager/src/Entities/Player.cpp
@@ -1,16 +1,75 @@
-// Player.cpp
-#include "Player.h"
 #include <algorithm> // Added to resolve std::find usage
+#include "Player.h"
 
-Player::Player() : name("Player"), status(), itemInventory(), abilityInventory() {}
+Player::Player(const std::string& name, int health, int mentalStrength, int attackPower, int money)
+    : name(name), health(health), mentalStrength(mentalStrength), attackPower(attackPower), money(money), itemInventory(), abilityInventory() {}
 
-Player::Player(const std::string& name, int strength, int health)
-    : name(name), status(), itemInventory(), abilityInventory() {
-    status.modifyHealth(health - 100); // Adjust initial health
+void Player::setName(const std::string& name) {
+    this->name = name;
+}
+
+std::string Player::getName() const {
+    return name;
+}
+
+void Player::setHealth(int health) {
+    this->health = health;
+}
+
+int Player::getHealth() const {
+    return health;
+}
+
+void Player::setMentalStrength(int mentalStrength) {
+    this->mentalStrength = mentalStrength;
+}
+
+int Player::getMentalStrength() const {
+    return mentalStrength;
+}
+
+void Player::setAttackPower(int attackPower) {
+    this->attackPower = attackPower;
+}
+
+int Player::getAttackPower() const {
+    return attackPower;
+}
+
+void Player::setMoney(int money) {
+    this->money = money;
+}
+
+int Player::getMoney() const {
+    return money;
 }
 
 void Player::modifyHealth(int amount) {
-    status.modifyHealth(amount);
+    setHealth(getHealth() + amount);
+    if (getHealth() < 0) {
+        setHealth(0);
+    }
+}
+
+void Player::modifyMentalStrength(int amount) {
+    setMentalStrength(getMentalStrength() + amount);
+    if (getMentalStrength() < 0) {
+        setMentalStrength(0);
+    }
+}
+
+void Player::modifyAttackPower(int amount) {
+    setAttackPower(getAttackPower() + amount);
+    if (getAttackPower() < 0) {
+        setAttackPower(0);
+    }
+}
+
+void Player::modifyMoney(int amount) {
+    setMoney(getMoney() + amount);
+    if (getMoney() < 0) {
+        setMoney(0);
+    }
 }
 
 void Player::addItem(const Item& item) {
