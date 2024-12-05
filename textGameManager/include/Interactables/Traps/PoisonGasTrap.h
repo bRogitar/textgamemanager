50bd3f8a39595009dbdaf18aa92f8d4573b7b6ad
jodoo1 <jodoo20@gmail.com>
2024-12-05 22:57:56 +0900

    npc trap

---
+++ textGameManager/include/Interactables/Traps/PoisonGasTrap.h
@@ -0,0 +1,19 @@
+#ifndef POISONGASTRAP_H
+#define POISONGASTRAP_H
+
+#include "BaseTrap.h"
+#include "Player.h"
+
+class PoisonGasTrap : public BaseTrap {
+public:
+    PoisonGasTrap(const std::string& name, int damage);
+    
+    void trigger(Player& player) override;
+    std::unique_ptr<BaseTrap> clone() const override;
+    bool canAvoid() const;
+
+private:
+    static const int DEFAULT_AVOID_CHANCE = 30;
+};
+
+#endif
